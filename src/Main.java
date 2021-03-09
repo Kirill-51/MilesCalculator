94d33221351716c6004fcb08e2f4e17804b64f1b
Kirill Popov <qer141@bk.ru>
2021-03-09 22:51:58 +0400

    add files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    boolean registered = true;
+    long	ticketprice = 130;
+    long  miles  = ticketprice/20;
+
+
+
+    System.out.println(miles);
+ }
+}
